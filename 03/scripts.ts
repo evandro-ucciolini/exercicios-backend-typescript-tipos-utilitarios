19c84948416a8c7cebf69aeca815b4e526030ec7
Evandro <evandro.ucciolini@outlook.com>
2024-05-04 14:51:43 -0300

    Exercício 03 feito

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -6,10 +6,25 @@ type Conn = {
     dbname: string
 }
 
-const conexao = (dados: Conn) => {
+type conexao = {
+    driver: string,
+    url: string
+}
+
+const conexao = (dados: Conn): Readonly<conexao> => {
     const { username, password, host, port, dbname } = dados;
     return {
         driver: 'postgres',
         url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
     };
+
 }
+
+const logado = conexao({
+    username: 'Evandro',
+    password: '1234',
+    host: 'teste',
+    port: '5432',
+    dbname: 'bdTeste'
+})
+
