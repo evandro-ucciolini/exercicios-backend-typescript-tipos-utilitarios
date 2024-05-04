ad7638929b73f80004f81fcdece467c059e97362
Evandro <evandro.ucciolini@outlook.com>
2024-05-04 13:59:20 -0300

    Exercício 01 feito

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -6,6 +6,15 @@ type Usuario = {
     dataNacimento?: string
 }
 
-const cadastrarUsuário = (info: Usuario): Usuario => {
+const cadastrarUsuário = (info: Partial<Usuario>) => {
     return info;
 }
+
+
+console.log(cadastrarUsuário({
+    nome: 'Evandro',
+    email: 'evandro@email.com',
+    cpf: '123456789-10',
+    dataNacimento: '18/12/1992'
+
+}));
