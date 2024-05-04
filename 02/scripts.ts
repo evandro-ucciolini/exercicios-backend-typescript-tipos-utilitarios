18564e3a53eb8b91af3cab78b77e542f2388b88e
Evandro <evandro.ucciolini@outlook.com>
2024-05-04 14:41:12 -0300

    Exercício 02 feito

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,25 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type endereco = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+
+}
+
+type Carrinho02 = {
+    item: Item,
+    qtd: number,
+    desconto: number,
+    frete: number,
+    tipoTransacao: Lowercase<'credito' | 'Debito'>,
+    cartao: Cartao,
+    endereco: endereco
+}
+
+
+
